90c1c8377237332fb07f25eae97bce5a47404778
OriCohen05 <79476840+OriCohen05@users.noreply.github.com>
2021-05-19 16:45:15 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,47 +1,9 @@
-//תמונות המשחק
-// createImagesArr();
-var img1 = document.getElementById("img1");
-var img2 = document.getElementById("img2");
-var img3 = document.getElementById("img3");
-var img4 = document.getElementById("img4");
-var img5 = document.getElementById("img5");
-var img6 = document.getElementById("img6");
-var img7 = document.getElementById("img7");
-var img8 = document.getElementById("img8");
-var img9 = document.getElementById("img9");
-var img10 = document.getElementById("img9");
-var img11 = document.getElementById("img11");
-var img12 = document.getElementById("img12");
-var img13 = document.getElementById("img13");
-var img14 = document.getElementById("img14");
-var img15 = document.getElementById("img15");
-var img16 = document.getElementById("img16");
-var img17 = document.getElementById("img17");
-var img18 = document.getElementById("img18");
-var img19 = document.getElementById("img19");
-var img20 = document.getElementById("img20");
-var img21 = document.getElementById("img21");
-var allHoles = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21];
 
-// var allHoles = [];
-// function createImagesArr() {
-//     allHoles = new Array(gBoardSize);
-//     for (var i = 1; i < 22; i++) 
-//         allHoles[i] += (document.getElementById(`img${i}`));
-
-// }
-
-
-
-//טבלאות המשחק
-var table1 = document.getElementById("gametable1");
-var table2 = document.getElementById("gametable2");
-var table3 = document.getElementById("gametable3");
 //אלמנטים חיצוניים
 var modalWrapper = document.getElementById("modal-wrapper");
 var modalElement = document.getElementById("modal-content");
 var modalTimer = document.getElementById("modal-timer");
-var gametable = document.getElementById("gametable");
+var gametable = document.getElementById("gametable1");
 var score = document.getElementById("Score");
 var startGameButton = document.getElementById("playbutton");
 var timer = document.getElementById("timer");
@@ -49,119 +11,192 @@ var hard = document.getElementById("hard");
 var medium = document.getElementById("medium");
 var easy = document.getElementById("easy");
 var gamediv = document.getElementById("GameDiv");
+var elemBestScore = document.getElementById('bestScore');
 //משתנים גלובליים
+var bestScore;
+var allHoles = [];
 var gcount = 0;
 var timeUp;
-var count = 0;
 var points = 0;
 var time;
 var gBoardSize = 0;
 var gFlag = false;
+var counter;
+var gPlayTime;
+var countDownInterval;
+function saveToStorage(key,val){
+    localStorage.setItem(key, JSON.stringify(val));
+}
 
-//פונקציה שמחזירה את כל החורים למצב הרגיל (רק חורים)
+function loadFromStorage(key){
+    var val = localStorage.getItem(key);
+    return JSON.parse(val);
+}
+
+
 function resetHoles() {
     for (var i = 0; i <= gBoardSize; i++) {
         allHoles[i].setAttribute('src', 'Images/hole.png');
     }
 }
-//פונקציה שמעלה את הניקוד על פי סורס
+
+function OnStartGame() {
+    
+    gametable.innerHTML = '';
+    BuildGameTable();
+    if (!gFlag) {
+        alert("Choose board before start!"); // Pop up
+        easy.disabled = false;
+        medium.disabled = false;
+        hard.disabled = false;
+        resetGame();        
+    } else {
+        gamediv.style.visibility = "visible"
+        startGameButton.disabled = true;
+        counter = 2;
+        modalElement.classList.toggle("hide");
+        countDownInterval = setInterval('countDown()', 1000);
+}
+}
+function GenerateHoles() {
+    resetHoles();
+    var randomHole = Math.floor(Math.random() * gBoardSize);
+    console.log(randomHole,allHoles);
+    allHoles[randomHole].setAttribute('src', 'Images/Logo.png');
+}
 function ClickToScore(object) {
     var source = object.src;
     if (source.search("Images/Logo.png") != -1) //מחפש את הסטרינג ומחזיר את האינדקס שהוא מתחיל בו בסטרינג שבו חיפשתי
     //-1 במקרה שלי הוא false
     {
-        count++;
-        score.innerHTML = `Your score: ${count} !`;
+        points++;
+        score.innerHTML = `Your score: ${points} !`;
         object.src = "Images/hole.png";
     }
 }
-//פונקציה שמגרילה חור רנדומלי
-function GenerateHoles() {
-    resetHoles();
-    var randomHole = Math.floor(Math.random() * 8);
-    //console.log(randomHole);
-    allHoles[randomHole].setAttribute('src', 'Images/Logo.png');
-    
+function BuildGameTable(){
+    var modalC = document.createElement('div');
+    var count = 1;
+    var placement = ''
+    var src = 'Images/hole.png';
+        modalC.setAttribute('id','modal-content');
+        for(var j = 1; j <= 3; j++){
+            if(j == 1){
+                placement = 'first';
+            }
+            else if(j == 2){
+                placement = 'second';
+            }
+            else if(j == 3){
+                placement = 'third';
+            }
+            var tr = document.createElement('tr');
+            tr.setAttribute('id', `${placement}row`);
+            console.log(gBoardSize);
+            for(var i = 1; i < gBoardSize / 2; i++){
+                var td = document.createElement('td');
+                td.setAttribute('class','gametds');
+                var img = document.createElement('img');
+                img.setAttribute('id',`img${count}`);
+                count++;
+                img.setAttribute('src', src);
+                img.setAttribute('onclick', 'ClickToScore(this)');
+                allHoles.push(img);
+                td.appendChild(img);
+                tr.appendChild(td);
+                console.log(gBoardSize);
+            }
+            gametable.append(tr);
+
+        }
 }
 
+
 function resetGame() {
+    alert("rs");
     count = 0;
+    gcount = 0;
     gBoardSize = 0;
     score.innerHTML = "";
+    allHoles = [];
     startGameButton.disabled = false;
     resetHoles();
     clearInterval(time);
     clearInterval(timeUp);
     gcount = 0;
     timer.innerHTML = "";
-    gamediv.innerHTML = '';
-    table1.style.display = "none";
-    table2.style.display = "none";
-    table3.style.display = "none";
+    gamediv.style.visibility = "hidden";
     gFlag = false;
+    alert("hey")
 }
+//פונקציה שמחזירה את כל החורים למצב הרגיל (רק חורים)
+
+//פונקציה שמעלה את הניקוד על פי סורס
+
+//פונקציה שמגרילה חור רנדומלי
+
 
 function roundTimer() {
     timer.innerHTML = "game timer: " + gcount;
     gcount += 1;
-    if (gcount === 31) {
-        gamediv.innerHTML = '';
-        alert("Time's up");
-        resetHoles();
-        resetGame();
+    if (gcount == 5) {
+        
+        if(points >= bestScore){
+            saveToStorage("bestScore", points);
+            elemBestScore.innerHTML = points; 
+        }
+        alert(); // **POP up alert soon / ignore this comment**
+        clearInterval(timeUp);
         timer.innerHTML = '';
+        count = 0;
+        gcount = 0;
+        allHoles = [];
+        gamediv.innerHTML = '';
+        gametable.innerHTML = '';
+        resetHoles();
+        gamediv.style.visibility = "hidden";
+        resetGame();
     }
 
 }
 
-function OnStartGame() {
-    if (!gFlag) {
-        alert("Choose board before start!");
-        easy.disabled = false;
-        medium.disabled = false;
-        hard.disabled = false;
-        resetGame();        
-    } else {
-        gamediv.style.display = "block";
-        startGameButton.disabled = true;
-        var counter = 5;
-        modalElement.classList.toggle("hide");
-        var countDown = setInterval(function() {
-            modalElement.innerHTML = counter;
+function countDown() {
+    console.log(counter);
+    modalElement.innerHTML = counter;
             counter--;
-            if (counter === -1) {
-                clearInterval(countDown);
+            if (counter == -1) {
+                console.log("hey");
+                clearInterval(countDownInterval);
                 modalElement.style.display = "none";
                 timeUp = setInterval(roundTimer, 1000);
-                time = setInterval(GenerateHoles, 2000);
+                time = setInterval(GenerateHoles, gPlayTime);
             }
-        }, 1000);
-    }
 }
+
 function onStartEasy() {
-    table1.style.display = "block";
-    // table2.style.display = "none";
-    // table3.style.display = "none";
-    gBoardSize = 8;
+    gBoardSize = 9;
     gFlag = true;
+    gPlayTime = 2000;
 }
 
-// function onStartMedium() {
-//     table2.style.display = "block";
-//     table3.style.display = "none";
-//     table1.style.display = "none";
-//     gBoardSize = 11;
-// }
+function onStartMedium() {
+    gBoardSize = 12;
+    gFlag = true;
+    gPlayTime = 1200;
+}
 
-// function onStartHard() {
-//     table3.style.display = "block";
-//     table1.style.display = "none";
-//     table2.style.display = "none";
-//     gBoardSize = 20;
-// }
+function onStartHard() {
+    gBoardSize = 21;
+    gFlag = true;
+    gPlayTime = 750;
+}
 
 function OnInit() {
     modalElement.classList.toggle("hide");
+    bestScore = loadFromStorage('bestScore');
+    if(!bestScore){
+        elemBestScore.innerHTML = 0;
+    } else elemBestScore.innerHTML = bestScore;  
 }
 
 console.info("All rights saved to Ori Cohen.");
